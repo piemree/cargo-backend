cc9b9ef7ddf0e479e0e906504e11676f95dcfaaa
piemree <pi.emree@gmail.com>
2023-04-08 06:12:30 +0300

    github workflow test

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const { loadRoutes, connectDb } = require("./helpers");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // create ping route
-app.get("/ping", (req, res) => res.send("pongg"));
+app.get("/ping", (req, res) => res.send("pongg1"));
 
 app.use(cors(config.corsOptions));
 app.use(auth);
