704bc48339bb780351bf0f24e5367e393ecad7b7
piemree <pi.emree@gmail.com>
2023-05-18 04:46:37 +0300

    allow cors

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ const auth = require("./middlewares/auth");
 const config = require("../config");
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(config.corsOptions));
+// allow cors to all origins
+app.use(cors());
 
 app.use(auth);
 
